57bb1c07f0832c7ca36ad31291d66ca385ebe5de
Dinesh Vasireddy <dineshvasireddy@college.harvard.edu>
2024-05-07 19:37:04 -0400

    styling stuff

--- src/components/experience.tsx
+++ src/components/experience.tsx
@@ -35,7 +35,7 @@ export default function Experience() {
           via-[#9c40ff] to-[#4060ff] bg-[length:var(--bg-size)_100%] 
           bg-clip-text text-transparent font-semibold`}
         >
-          Building in NYC (June-)
+          Building
         </span>
       </AnimatedGradientText>
       
